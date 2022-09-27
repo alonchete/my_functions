9022b807221b76467ab39dde8d2cc09665150068
alonchete <alonsete.tyr@gmail.com>
2022-09-27 20:25:21 +0200

    mapa

--- map.js
+++ map.js
@@ -1,4 +1,22 @@
-var p;
-var s;
-var l;
 
+function map (xs, f){
+    var n = [];
+    for(var i = 0; i < xs.length; i++){
+        n.push(f(xs[i]));
+    }
+    return n;
+}
+
+function suma(ns){
+    return ns + 1;
+}
+
+function mayus(ns){
+    return ns.slice(1);
+}
+
+var number = [1,2,3,4,5] 
+
+
+console.log(map(number, suma));
+console.log(map(["CARLOS", "MARCO"], mayus));
